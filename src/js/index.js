63e93289f3c3726863b866ba3dd06d340cd9fed4
farhan <farhan7534031b@gmail.com>
2021-11-23 15:41:30 +0700

    remove unnecessary think
    - moment js
    - contact html
    - img
    - sass

--- src/js/index.js
+++
@@ -1,9 +0,0 @@
-import "core-js";
-import "../sass/main.scss";
-import { multiplyNum } from "./utils/multipleNumber.js";
-
-const array1 = [5, 12, 8, 130, 44];
-
-const found = array1.find((element) => element > 10);
-
-console.log(found);
--- src/js/utils/multipleNumber.js
+++
@@ -1,5 +0,0 @@
-export const multiplyNum = (...num) => {
-    return num.reduce((result, currVal) => {
-        return result * currVal;
-    });
-};
